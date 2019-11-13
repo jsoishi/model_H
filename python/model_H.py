edeb9abc41899df0f47e652f5d0dd180ece5892b
J. S. Oishi <jsoishi@gmail.com>
2019-11-13 17:04:14 -0500

    Many fixes for usability.
    * kappa_hat != 0 still not working

--- python/model_H.py
+++ python/model_H.py
@@ -6,7 +6,7 @@ Pass it a .cfg file to specify parameters for the particular solve.
 To run using 4 processes, you would use:
     $ mpiexec -n 4 python3 mri.py mri_params.cfg
 """
-
+import os
 import time
 import configparser
 from configparser import ConfigParser
@@ -15,7 +15,11 @@ import numpy as np
 import sys
 import h5py
 import dedalus.public as de
+from dedalus.tools  import post
+from dedalus.extras import flow_tools
+
 from mpi4py import MPI
+from filter_field_iso import filter_field
 
 CW = MPI.COMM_WORLD
 import logging
@@ -23,24 +27,26 @@ import logging
 logger = logging.getLogger(__name__)
 
 # Parses .cfg filename passed to script
-filename = Path(sys.argv[-1])
-outbase = Path("data")
+config_file = Path(sys.argv[-1])
 
 # Parse .cfg file to set global parameters for script
 config = ConfigParser()
-config.read(str(filename))
+config.read(str(config_file))
 
 logger.info('Running model_H.py with the following parameters:')
 logger.info(config.items('parameters'))
 
-Nx = config.getint('parameters', 'Nx')
-Ny = config.getint('parameters', 'Ny')
-kappa = config.getfloat('parameters', 'kappa')
-kappa_hat = config.getfloat('parameters', 'kappa_hat')
+params = config['parameters']
+Nx = params.getint('Nx')
+Ny = params.getint('Ny')
 
-a = config.getfloat('parameters', 'a')
-b = config.getfloat('parameters', 'b')
-
+kappa = params.getfloat('kappa')
+kappa_hat = params.getfloat('kappa_hat')
+nu = params.getfloat('nu')
+a = params.getfloat('a')
+b = params.getfloat('b')
+ampl = params.getfloat('ampl')
+seed = params.getint('seed')
 
 # Create bases and domain
 # Use COMM_SELF so keep calculations independent between processes
@@ -50,7 +56,6 @@ y_basis = de.Fourier('y', Ny, interval=(0., Ny))
 domain = de.Domain([x_basis, y_basis], grid_dtype=np.float)
 
 # Active Matter Model H
-
 problem_variables = ['p', 'vx', 'vy', 'phi']
 problem = de.IVP(domain, variables=problem_variables)
 
@@ -58,10 +63,10 @@ problem = de.IVP(domain, variables=problem_variables)
 
 problem.parameters['a'] = a
 problem.parameters['b'] = b
-problem.parameters["ll"] = ll
+#problem.parameters["ll"] = ll
 problem.parameters['kappa'] = kappa
 problem.parameters['kappa_hat'] = kappa_hat
-problem.parameters['ν'] = ν
+problem.parameters['ν'] = nu
 
 # Operator substitutions for Laplacian
 problem.substitutions['Lap(A)'] = "dx(dx(A))+dy(dy(A))"
@@ -70,38 +75,63 @@ problem.substitutions['Lap(A)'] = "dx(dx(A))+dy(dy(A))"
 problem.substitutions['phi_x'] = 'dx(phi)'
 problem.substitutions['phi_y'] = 'dy(phi)'
 problem.substitutions['grad_phi_2'] = 'phi_x**2 + phi_y**2'
-# Hydro equations: p, vx, vz
 
-problem.add_equation("dx(vx) + dy(vy) = 0")
-problem.add_equation("dt(vx) + dx(p) - ν*Lap(vx) = -vx*dx(vx) - vy*dy(vx) - kappa_hat*(dx(phi_x**2) - 0.5*dx(grad_phi_2) + dy(phi_x*phi_y))")
-problem.add_equation("dt(vy) + dy(p) - ν*Lap(vx) = -vx*dx(vy) - vy*dy(vy) - kappa_hat*(dy(phi_y**2) - 0.5*dy(grad_phi_2) + dx(phi_x*phi_y))")
+# Hydro equations: p, vx, vz
+problem.add_equation("dx(vx) + dy(vy) = 0", condition="(nx != 0) or (ny != 0)")
+problem.add_equation("p = 0", condition="(nx == 0) and (ny == 0)")
+
+problem.add_equation("dt(vx) + dx(p) - ν*Lap(vx) = -vx*dx(vx) - vy*dy(vx) - kappa_hat*(2*dx(phi)*dx(dx(phi)) + dx(dy(phi))*dy(phi) +   dx(phi)*dy(dy(phi)) - dx(Lap(phi)**2)/2.)")
+problem.add_equation("dt(vy) + dy(p) - ν*Lap(vx) = -vx*dx(vy) - vy*dy(vy) - kappa_hat*(  dx(dy(phi))*dx(phi) + dy(phi)*dx(dx(phi)) + 2*dy(phi)*dy(dy(phi)) - dy(Lap(phi)**2)/2.)")
+
 
 # Phase field
 problem.add_equation("dt(phi) + kappa*Lap(Lap(phi)) = -vx*dx(phi) - vy*dy(phi) + a*Lap(phi) + b*Lap(phi**3)")
 
 solver = problem.build_solver(de.timesteppers.SBDF2)
 
+# create data dir using basename of cfg file
+basedir = Path('scratch')
+outdir = "run_" + config_file.stem
+data_dir = basedir/outdir
+if domain.dist.comm.rank == 0:
+    if not data_dir.exists():
+        data_dir.mkdir(parents=True)
+
 # Analysis
 analysis_tasks = []
-snap = solver.evaluator.add_file_handler(os.path.join(data_dir,'snapshots'), iter=100, max_writes=200)
+snap = solver.evaluator.add_file_handler(os.path.join(data_dir,'snapshots'), iter=1, max_writes=200)
 snap.add_task("phi")
 snap.add_task("vx")
 snap.add_task("vy")
 analysis_tasks.append(snap)
 
 # Create initial conditions
-gshape = ch.domain.dist.grid_layout.global_shape(scales=ch.domain.dealias)
-slices = ch.domain.dist.grid_layout.slices(scales=ch.domain.dealias)
-rand = np.random.RandomState(seed=100)
+gshape = problem.domain.dist.grid_layout.global_shape(scales=problem.domain.dealias)
+slices = problem.domain.dist.grid_layout.slices(scales=problem.domain.dealias)
+rand = np.random.RandomState(seed=seed)
 noise = rand.standard_normal(gshape)[slices]
 
 phi0 = solver.state['phi']
 phi0['g'] = ampl*noise
 filter_field(phi0)
 
-solver.stop_wall_time = 24*3600
-solver.stop_sim_time = 8000.
-solver.stop_iteration = np.inf#100
+run_opts = config['run']
+dt = run_opts.getfloat('dt')
+
+if run_opts.getfloat('stop_wall_time'):
+    solver.stop_wall_time = run_opts.getfloat('stop_wall_time')
+else:
+    solver.stop_wall_time = np.inf
+
+if run_opts.getint('stop_iteration'):
+    solver.stop_iteration = run_opts.getint('stop_iteration')
+else:
+    solver.stop_iteration = np.inf
+
+if run_opts.getfloat('stop_sim_time'):
+    solver.stop_sim_time = run_opts.getfloat('stop_sim_time')
+else:
+    solver.stop_sim_time = np.inf
 
 flow = flow_tools.GlobalFlowProperty(solver, cadence=10)
 flow.add_property("integ(phi)", name="Cint")
